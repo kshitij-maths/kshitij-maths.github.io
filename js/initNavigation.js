6f9882a4ff7429b6f130a957a47a2db923f13b10
kshitij-maths <kshitij-maths@users.noreply.github.com>
2026-05-30 12:33:38 +0000

    deploy: kshitij-maths/Webpage@92902a3b3d5317020373f58180e2d79db58282a8

--- js/initNavigation.js
+++
@@ -1,9 +0,0 @@
-export function initNavigation() {
-  const links = document.querySelectorAll(".top-link");
-  links.forEach(link => {
-    link.addEventListener("click", () => {
-      const menu = document.getElementById("mobileMenu");
-      if (menu) menu.classList.add("hidden");
-    });
-  });
-}
